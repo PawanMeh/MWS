ce4c6d938dbc88bd970fbd44a774afc0d6dbe77e
Pawan Mehta <pawan@openetech.com>
2020-05-22 12:46:34 +0530

    fix: amounts

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 import frappe, json, time, datetime, dateutil, math, csv, StringIO
+from frappe.utils import flt
 import amazon_mws as mws
 from frappe import _
 
@@ -699,7 +700,7 @@ def get_postal_fees(market_place_order_id):
 			for adjustment in adjustment_event_list:
 				if 'AdjustmentType' in adjustment.keys():
 					if (adjustment.AdjustmentType == "PostageBilling_Postage" or adjustment.AdjustmentType == "PostageBilling_SignatureConfirmation"):
-						charges_fees += adjustment.AdjustmentAmount.CurrencyAmount
+						charges_fees += flt(adjustment.AdjustmentAmount.CurrencyAmount)
 
 	frappe.msgprint("postal fees")
 	frappe.msgprint(charges_fees)	
