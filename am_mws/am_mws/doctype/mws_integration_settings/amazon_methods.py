913b6694887bd36bf6be124e8cf60f59d2fd973a
Pawan Mehta <pawan@openetech.com>
2020-05-22 15:43:44 +0530

    remove msgprint

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -700,7 +700,6 @@ def get_postal_fees(market_place_order_id):
 			for adjustment in adjustment_event_list:
 				if 'AdjustmentType' in adjustment.keys():
 					if (adjustment.AdjustmentType == "PostageBilling_Postage" or adjustment.AdjustmentType == "PostageBilling_SignatureConfirmation"):
-						frappe.msgprint(adjustment.AdjustmentAmount.CurrencyAmount)
 						fees += flt(adjustment.AdjustmentAmount.CurrencyAmount)
 
 	return fees
