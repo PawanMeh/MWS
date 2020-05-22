4ddb3eaaadf4c21f5d800f8dfd3abf34a41ecdb4
Pawan Mehta <pawan@openetech.com>
2020-05-22 21:15:18 +0530

    fix: je

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -648,9 +648,7 @@ def get_order_create_label_jv(after_date):
 		if not je_exists:
 			fees_dict = get_postal_fees(order['market_place_order_id'])
 			#create JV
-			if fees_dict.get('fees') > 0:
-				frappe.msgprint("create jv")
-				jv_no = create_jv(order['market_place_order_id'], order['transaction_date'], fees[0] * -1)
+			jv_no = create_jv(order['market_place_order_id'], order['transaction_date'], fees_dict.get('fees') * -1)
 
 def create_jv(market_place_order_id, transaction_date, fees):
 	company = frappe.db.get_value("MWS Integration Settings", "MWS Integration Settings", "company")
