0ab292335b611ede1a5f898d80430c68160246a1
Pawan Mehta <pawan@openetech.com>
2020-06-17 16:52:27 +0530

    debug: adjustment detail

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -712,16 +712,15 @@ def create_jv(market_place_order_id, transaction_date, fees):
 def get_postal_fees(market_place_order_id):
 	finances = get_finances_instance()
 	response = call_mws_method(finances.list_financial_events, amazon_order_id=market_place_order_id)
+	frappe.msgprint(response)
 	adjustment_events = return_as_list(response.parsed.FinancialEvents.AdjustmentEventList)
-
+	frappe.msgprint(adjustment_events)
 	total_fees = 0
 	for adjustment_event in adjustment_events:
 		if adjustment_event:
 			adjustment_event_list = return_as_list(adjustment_event.AdjustmentEvent)
-			frappe.msgprint(adjustment_event_list)
 			for adjustment in adjustment_event_list:
 				if 'AdjustmentType' in adjustment.keys():
-					frappe.msgprint("adjustment keys")
 					if (adjustment.AdjustmentType == "PostageBilling_Postage" or adjustment.AdjustmentType == "PostageBilling_SignatureConfirmation"):
 						total_fees += flt(adjustment.AdjustmentAmount.CurrencyAmount)
 				else:
