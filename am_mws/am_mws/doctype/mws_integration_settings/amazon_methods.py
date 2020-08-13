6fc9442b79490bed3c382c59441face2a6001402
Pawan Mehta <pawan@openetech.com>
2020-08-13 16:06:16 +0530

    fix: amazon

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -657,8 +657,7 @@ def get_charges_and_fees(market_place_order_id):
 def get_orders_create_refund(after_date):
 	try:
 		orders = get_orders_instance()
-		#statuses = ["PartiallyShipped", "Unshipped", "Shipped", "Canceled"]
-		statuses = ["Canceled"]
+		statuses = ["PartiallyShipped", "Unshipped", "Shipped", "Canceled"]
 		mws_settings = frappe.get_doc("MWS Integration Settings")
 		market_place_list = return_as_list(mws_settings.market_place_id)
 
