488a77e33c3340880b25c4eadf8ed3d3538dd9d9
Pawan Mehta <pawan@openetech.com>
2020-08-18 00:00:35 +0530

    fix: pos

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -1075,9 +1075,10 @@ def create_return_invoice(args):
 			se.insert(ignore_mandatory=True, ignore_permissions=True)
 			mode_of_payment = frappe.db.get_value("MWS Integration Settings", "MWS Integration Settings", "mode_of_payment")
 			se.append('payments', {"mode_of_payment": mode_of_payment, 
-									"amount": -1 * se.outstanding_amount, 
-									"base_amount":-1 * se.outstanding_amount})
-			se.paid_amount = -1 * se.outstanding_amount
+									"amount": se.outstanding_amount, 
+									"base_amount":se.outstanding_amount})
+			se.paid_amount = se.outstanding_amount
+			se.outstanding_amount = 0
 			se.save(ignore_permissions=True)
 			if mws_settings.submit_credit_invoice:
 				se.submit()
