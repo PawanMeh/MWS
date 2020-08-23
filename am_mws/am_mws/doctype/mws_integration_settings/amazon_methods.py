48685f5cf3f60874baa4a3779d3a803856f120e2
Pawan Mehta <pawan@openetech.com>
2020-08-23 23:14:37 +0530

    fix: totals

--- am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
+++ am_mws/am_mws/doctype/mws_integration_settings/amazon_methods.py
@@ -818,53 +818,56 @@ def create_return_jv(se_args):
 	company = frappe.db.get_value("MWS Integration Settings", "MWS Integration Settings", "company")
 	mode_of_payment = frappe.db.get_value("MWS Integration Settings", "MWS Integration Settings", "mode_of_payment")
 	args = frappe._dict(se_args)
-	#credit mop
-	#debit charges account
-	credit_account = frappe.db.sql('''
-						select
-							default_account
-						from
-							`tabMode of Payment Account`
-						where
-							company = %s and
-							parent = %s
-						''', (company, mode_of_payment))
-	#create jv
-	je_doc = frappe.new_doc("Journal Entry")
-	je_doc.company = company
-	je_doc.voucher_type = "Journal Entry"
-	je_doc.cheque_no = args.market_place_order_id
-	je_doc.posting_date = args.posting_date
-	je_doc.cheque_date = args.posting_date
-	cost_center = "Main - SHM"
-	tot_amount = 0
-	for charge in args.taxes:
-		if flt(charge['tax_amount']) < 0:
-			je_doc.append("accounts", {
-				"account": charge['account_head'],
-				"cost_center": cost_center,
-				"debit_in_account_currency": flt(charge['tax_amount']) * -1,
-				"debit": flt(charge['tax_amount']) * -1,
-				"credit_in_account_currency": 0,
-				"credit": 0
-			})
-			tot_amount += flt(charge['tax_amount']) * -1
-	je_doc.append("accounts", {
-		"account": credit_account[0][0],
-		"cost_center": cost_center,
-		"debit_in_account_currency": 0,
-		"debit": 0,
-		"credit_in_account_currency": tot_amount,
-		"credit": tot_amount
-	})
-	try:
-		je_doc.insert(ignore_permissions=True)
-		if je_doc.total_debit == 0 and je_doc.total_credit == 0:
-			frappe.delete_doc('Journal Entry', je_doc.name)
-		else:
-			return je_doc.name
-	except Exception as e:
-		frappe.log_error(message=e, title="Return JV Error" + je_doc.cheque_no + je_doc.posting_date.strftime('%Y-%m-%d'))
+	if frappe.db.exists({'doctype': 'Journal Entry','cheque_no': args.market_place_order_id}):
+		pass
+	else:
+		#credit mop
+		#debit charges account
+		credit_account = frappe.db.sql('''
+							select
+								default_account
+							from
+								`tabMode of Payment Account`
+							where
+								company = %s and
+								parent = %s
+							''', (company, mode_of_payment))
+		#create jv
+		je_doc = frappe.new_doc("Journal Entry")
+		je_doc.company = company
+		je_doc.voucher_type = "Journal Entry"
+		je_doc.cheque_no = args.market_place_order_id
+		je_doc.posting_date = args.posting_date
+		je_doc.cheque_date = args.posting_date
+		cost_center = "Main - SHM"
+		tot_amount = 0
+		for charge in args.taxes:
+			if flt(charge['tax_amount']) < 0:
+				je_doc.append("accounts", {
+					"account": charge['account_head'],
+					"cost_center": cost_center,
+					"debit_in_account_currency": flt(charge['tax_amount']) * -1,
+					"debit": flt(charge['tax_amount']) * -1,
+					"credit_in_account_currency": 0,
+					"credit": 0
+				})
+				tot_amount += flt(charge['tax_amount']) * -1
+		je_doc.append("accounts", {
+			"account": credit_account[0][0],
+			"cost_center": cost_center,
+			"debit_in_account_currency": 0,
+			"debit": 0,
+			"credit_in_account_currency": tot_amount,
+			"credit": tot_amount
+		})
+		try:
+			je_doc.insert(ignore_permissions=True)
+			if je_doc.total_debit == 0 and je_doc.total_credit == 0:
+				frappe.delete_doc('Journal Entry', je_doc.name)
+			else:
+				return je_doc.name
+		except Exception as e:
+			frappe.log_error(message=e, title="Return JV Error" + je_doc.cheque_no + je_doc.posting_date.strftime('%Y-%m-%d'))
 
 
 def get_order_create_label_jv(after_date):
